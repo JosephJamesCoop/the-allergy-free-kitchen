45b470a5eb1f51289a1281655f5244311d875f4f
DrDano <danielharned@gmail.com>
2022-02-15 12:04:21 -0800

    adding initial imports for recipe model

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,1 +1,5 @@
 // This model will be populated with recipes (posts that users make) and will be connected with both the Ingredient and Allergy models.
+
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
