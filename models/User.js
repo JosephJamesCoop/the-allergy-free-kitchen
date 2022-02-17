e13a5b4a9cb54ff3411393d5d5b53727fa83020b
DrDano <danielharned@gmail.com>
2022-02-16 21:27:39 -0800

    formatting change

--- models/User.js
+++ models/User.js
@@ -56,7 +56,7 @@ User.init(
             updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
             return updatedUserData;
         }
-    },
+    }, 
     sequelize,
     timestamps: false,
     freezeTableName: true,
