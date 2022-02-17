711373aec33c936b04db4cfc42a18fa80cd1553c
Waffles <knharman@gmail.com>
2022-02-16 21:27:59 -0800

    icons display for recipes

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('homepage')
+    res.render('homepage', {
+        recipes: [
+            {
+                name: 'fried chicken',
+                icons: ['dairy', 'shellfish']
+            }, 
+            {
+                name: 'shrimp tacos',
+                icons: ['soy']
+            }
+        ]
+    })
 })
 
 module.exports = router;
