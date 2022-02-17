5cd80a2c9e335d2296245fd63273bcc6622f24e9
Heather Violet <heatherviolet91@gmail.com>
2022-02-16 20:18:13 -0800

    user model added

--- models/User.js
+++ models/User.js
@@ -6,8 +6,59 @@ const sequelize = require('../config/connection');
 const Recipe = require('../models/Recipe');
 const Vote = require('../models/Vote');
 const Allergy = require('../models/Allergy');
+const { beforeUpdate } = require('../models/Vote');
 
-class User extends Model {}
+class User extends Model {
+    // sets up method to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
-User.init
+User.init(
+    {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6]
+            }
+        },
+    },
+    {
+    hooks: {
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+    }
+);
 // User is connected with Allergy so that each user can list their allergies and be recommended recipes by the site that don't contain their allergens.
