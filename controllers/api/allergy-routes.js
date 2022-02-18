6692cd8e66640d5ac6082c8ebbba269bd84fd1b8
DrDano <danielharned@gmail.com>
2022-02-17 18:22:07 -0800

    merging allergy model into branch and writing get all route in allergy routes

--- controllers/api/allergy-routes.js
+++ controllers/api/allergy-routes.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
-const { Recipe, User, UPVote, DownVote, Comment } = require('../../models');
+const { Recipe, User, Vote, DownVote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
+router.get('/', async (req, res) => {
+    try {
+        console.log('=============================');
+        const allergyData = await Allergy.findAll({
+            attributes: [
+                'id',
+                'name',
+                'description',
+                'ingredients'
+            ],
+            include: [
+                {
+                    model: Recipe,
+                    attributes: ['id', 'name', 'ingredients'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        });
 
+        res.json(allergyData);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
