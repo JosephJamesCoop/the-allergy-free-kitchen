57bc6009d77555820a2d17d5e6fff5b418740cb7
Waffles <knharman@gmail.com>
2022-02-18 10:48:18 -0800

    seeds.sql test data

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO allergy (name, description, indredients)
+VALUES 
+  ("Dairy", "Dairy products are derived from milk", "milk, cheese, butter, eggs, yogurt"),
+  ("Soy", "null", "soybeans, soymilk, tofu, miso, tempeh"), 
+  ("Nut", "null", "walnut, almond, hazelnut, pecan, cashew pistachio, coconut"), 
+  ("Celiac Disease" "An immune reaction to eating gluten. Many more products than you may think contain gluten.", "wheat, barley, rye"),
+  ("Shellfish", "Mollusks", "shrimp, crab, lobster, clams, scallops, oysters, mussels"), 
+  ("Vegetarian", "No meat or meat derivitives", "beef, chicken, lamb, pork")
+
+  INSERT INTO recipe (name, description, indredients)
+  VALUES
+  ("shrimp tacos", "delish shrimp tacos", "1.season shrimp and fry 2. add toppings", "shrimp, tortillas, lime, avocado, cabbage, cilantro"), 
+  ("tofu scramble", "delish tofu scramble", "1.drain and press tofu 2.crumble tofu and season 3. scramble in a pan and add veggies", "tofu, onions, spinach, peppers"), 
+  ("peanut butter", "delish homemade peanut butter", "1.grind peanuts in food processor until smooth 2.add salt to taste", "peanuts, salt"), 
+  ("feta dip", "delish feta dip", "1.combine feta, olive oil, and herbs 2.stir", "feta, olive oil, basil, thyme"), 
+  ("fried chicken", "delish fried chicken", "1.dredge chicken in buttermilk, then flour")
