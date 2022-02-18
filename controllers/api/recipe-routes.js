298ec217e4048535a7403c15f3f465db232de0f2
DrDano <danielharned@gmail.com>
2022-02-18 15:52:17 -0800

    adding ingredientsClean back into recipe-routes

--- controllers/api/recipe-routes.js
+++ controllers/api/recipe-routes.js
@@ -8,7 +8,7 @@ const sequelize = require('../../config/connection');
 router.get('/', (req, res) => {
   Recipe.findAll({
     attributes: [
-      'id', 'name', 'description', 'instructions', 'ingredients', 'user_id',
+      'id', 'name', 'description', 'instructions', 'ingredients', 'ingredientsClean', 'user_id',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE recipe.id = vote.recipe_id)'), 'vote_count']
     ],
     order: [['created_at', 'DESC']],
