cca1ece145c59b87dbef1b5ef4e410cc1303e3cb
JosephJamesCoop <cooper_3869@live.com>
2022-02-17 21:36:25 -0800

    user, index, and vote are working

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
+const apiRoutes = require("./api");
 const homepageRoutes = require('./homepage-routes');
 
+router.use("/api", apiRoutes);
 router.use('/', homepageRoutes);
 
 module.exports = router;
