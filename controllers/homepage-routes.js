2759322b069adafb0cabed066b37864fe33c790c
Waffles <knharman@gmail.com>
2022-02-18 11:57:06 -0800

    cleanup

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -37,23 +37,6 @@ router.get("/", (req, res) => {
     });
 });
 
-// router.get('/', (req, res) => {
-//     res.render('homepage', {
-//         recipes: [
-//             {
-//                 name: 'fried chicken',
-//                 icons: ['dairy', 'shellfish'],
-//                 description: 'yum yum delish fried chicken'
-//             }, 
-//             {
-//                 name: 'shrimp tacos',
-//                 icons: ['soy'],
-//                 description: 'boy howdy dems are some good shrimp tacos'
-//             }
-//         ]
-//     })
-// })
-
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
