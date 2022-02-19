af93f5f5bd374353198fbf014160f914e3396788
Heather Violet <heatherviolet91@gmail.com>
2022-02-19 15:36:47 -0800

    started add recipe js and add vote js

--- public/javascript/add-recipe.js
+++ public/javascript/add-recipe.js
@@ -1,5 +1,13 @@
 async function recipeFormHandler(event) {
     event.preventDefault();
 
-    const 
+    const recipe_post = document.querySelector('textarea[name="recipe-body"]').value.trim();
+    
+    const recipe_id = window.loction.toString().split('/')[
+        window.loction.toString().split('/').length - 1
+    ];
+
+    console.log(recipe_post, recipe_id);
 }
+
+document.querySelector('.recipe-form').addEventListener('submit', recipeFormHandler);
