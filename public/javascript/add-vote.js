93da60e6de9ea34a79be7cfc9e74564c42d919cb
Heather Violet <heather@heathers-air.lan>
2022-02-19 15:36:47 -0800

    started add recipe js and add vote js

--- public/javascript/add-vote.js
+++ public/javascript/add-vote.js
@@ -1,7 +1,21 @@
 async function voteClickHandler(event) {
     event.preventDefault();
 
-    console.log('button clicked');
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch('/api/vote/vote-routes', {
+        method: 'PUT',
+        body: JSON.stringify({
+            post_id: id
+        }),
+    })
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert(response.statusText);
+    }
 }
 
 document.querySelector('.vote-btn').addEventListener('click', voteClickHandler);
