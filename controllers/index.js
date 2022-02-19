fb2f76493dfa12f7bd6d591ff96b86c79e26a6e6
JosephJamesCoop <cooper_3869@live.com>
2022-02-19 10:09:49 -0800

    userprofile created

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require("./api");
 const homepageRoutes = require('./homepage-routes');
+const userprofileRoutes = require('./userprofile-routes');
 
 router.use("/api", apiRoutes);
 router.use('/', homepageRoutes);
+router.use('/userprofile', userprofileRoutes);
 
 module.exports = router;
