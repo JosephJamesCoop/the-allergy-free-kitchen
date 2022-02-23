e2c2645bfa7b024efd6b71370d7c4653c8057b67
Kellie Harman <knharman@gmail.com>
2022-02-22 18:22:34 -0800

    routing fixes to create recipe

--- public/javascript/add-recipe.js
+++ public/javascript/add-recipe.js
@@ -1,38 +1,25 @@
-//const recipe_post_l = document.querySelector('textarea[name="recipe-body"]');
-async function recipe() {
-    document.location.replace('/api/recipe');
-  }
-  
-  document.querySelector('#add-recipe').addEventListener('click', recipe);
-
 async function recipeFormHandler(event) {
-    event.preventDefault();
-    
- //  const recipe_id = window.loction.toString().split('/')[
- //   window.loction.toString().split('/').length - 1
-//];
+  event.preventDefault();
+  
+  const title = document.querySelector('input[name="recipe-title"]').value;
+  const description = document.querySelector('input[name="recipe-description"]').value;
+  const ingredients = document.querySelector('input[name="recipe-ingredients"]').value;
+  const instructions = document.querySelector('input[name="recipe-instructions"]').value;
+  if (title && description && ingredients && instructions) {
 
-  //  const recipe_post = recipe_post_l.value.trim();
-    const title = document.querySelector('input[name="recipe-title"]').value;
-    const description = document.querySelector('input[name="recipe-description"]').value;
-    const ingredients = document.querySelector('input[name="recipe-ingredients"]').value;
-    const instructions = document.querySelector('input[name="recipe-instructions"]').value;
- 
-    if (title && description && ingredients && instructions) {
     const response = await fetch(`/api/recipes`, {
-        method: 'POST',
-        body: JSON.stringify({
-            title,
-            description,
-            ingredients,
-            instructions
-        }),
-        headers: { 
-            'Content-Type': 'application/json'}
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        description,
+        ingredients,
+        instructions
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     })
-    // create object and fetch method/post
 
-      
     if (response.ok) {
       document.location.replace('/userprofile');
     } else {
@@ -41,4 +28,4 @@ async function recipeFormHandler(event) {
   }
 }
 
-document.querySelector('add-recipe-form').addEventListener('click', recipeFormHandler);
+document.querySelector('.add-recipe-form').addEventListener('submit', recipeFormHandler);
